02ea56287273a19bfff2599073a2bfd8f3272017
behcet <behcet@DESKTOP-QDP1QTN>
2022-10-21 10:52:21 +0300

    Add project files.

---
+++ udpClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace udpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = "27000";
+            while (key != "!")
+            {
+
+                UDPSocket c = new UDPSocket();
+                c.Client("127.0.0.1", Convert.ToInt32(key));
+                c.Send("TEST!");
+
+
+                key = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ udpServer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace udpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Thread[] array = new Thread[20];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                ParameterizedThreadStart start = new ParameterizedThreadStart(Start);
+                array[i] = new Thread(start);
+                array[i].Start(i);
+
+            }
+
+            // Join all the threads.
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i].Join();
+            }
+
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+
+        static void Start(object i)
+        {
+            UDPSocket s;
+
+            s = new UDPSocket(i);
+            s.Server("127.0.0.1", 27000 + Convert.ToInt32(i));
+
+            Console.WriteLine($"{i} kimlik nolu ekran başlatıldı.");
+        }
+    }
+}
